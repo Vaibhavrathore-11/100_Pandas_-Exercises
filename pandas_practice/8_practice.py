85ddc614f4dc5c445fec1b289446012387b384b1
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-08-23 22:00:23 +0530

    python -u

---
+++ pandas_practice/8_practice.py
@@ -0,0 +1,5 @@
+#Create a dataFrame using list of lists
+
+import pandas as pd
+df = pd.DataFrame([["Vaibhav", 20], ["Isha",20], ["kashu" , 22], ["Aadi", 18]], columns = ["Name" , "Age"])
+print(df)
