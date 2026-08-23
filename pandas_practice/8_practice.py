08d1b79f17636596410a501c3141f4ebdeef7532
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-08-23 22:04:43 +0530

    Create a DataFrame using dictionary

--- pandas_practice/8_practice.py
+++ pandas_practice/8_practice.py
@@ -1,4 +1,4 @@
-#Create a dataFrame using list of lists
+#Create a DataFrame using dictionary
 
 import pandas as pd
 df = pd.DataFrame([["Vaibhav", 20], ["Isha",20], ["kashu" , 22], ["Aadi", 18]], columns = ["Name" , "Age"])
