29d568c80e6b5e7f9cb172937ef30c2dc0326eb2
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-02 18:34:11 +0530

    return the num of rows and columns using-> shapr()

---
+++ shape.py
@@ -0,0 +1,4 @@
+#shape  returns the number of rows and columns in the dataframe
+import pandas as pd 
+df = pd.read_csv("students1.csv")
+print(df.shape) # returns the number of rows and columns in the dataframe
