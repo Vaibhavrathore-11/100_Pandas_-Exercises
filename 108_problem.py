66132e319aaa0a5488dafd43afbc332dcb88bfe4
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-02 17:43:44 +0530

    tail() to get the last five rows

---
+++ 108_problem.py
@@ -0,0 +1,8 @@
+# tail method to get the last five rows of the dataframe
+import pandas as pd
+df = pd.read_csv("students1.csv")
+print(df.tail()) # last 5 rows
+
+import pandas as pd 
+df = pd.read_csv("Test.csv")
+print(df.tail()) # last five rows
