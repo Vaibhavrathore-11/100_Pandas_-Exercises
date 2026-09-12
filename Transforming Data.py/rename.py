0b40fd62ebb675ac37a59cb32a260af63ed4de8a
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-12 12:07:21 +0530

    Transforming Data, using rename() , Date time functions

---
+++ Transforming Data.py/rename.py
@@ -0,0 +1,16 @@
+import pandas as pd
+data = {
+    "Name" :["heery", "somya", "Nandu"],
+    "Age" :[21, 23, 11],
+    "Salary" :[10000,20000,30000]
+}
+df = pd.DataFrame(data)
+print(df)
+
+df = df.rename(columns = {"Salary" : "income"}) # columns rename karna [df.rename(columns)]
+
+df = df.rename(columns = {"Name": "Employee_Name"})
+print(df)
+
+df = df.rename(index = {0:"zero"})
+print(df)# index ka use karke label rename karte ha 
