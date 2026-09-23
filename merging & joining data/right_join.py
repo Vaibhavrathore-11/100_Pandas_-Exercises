eae1943782b738ca0c2287b2832bf93b35e17ffe
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-23 17:54:13 +0530

    Practice right join with employee and department data

---
+++ merging & joining data/right_join.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df_employees = pd.DataFrame({
+    "employee_id" :[101,102,103,104,105],
+    "name": ["rahul","Aman","anshika","radhe","gogi"],
+    "department_id": [1,2,1,3,5],
+    "salary":[35000,42000,38000,45000,40000]
+})
+
+df_departments = pd.DataFrame({
+    "department_id": [1,2,3,4],
+    "department_name": ["IT","HR","Finance","Marketing"],
+    "Location": ["Indore","Bhopal","indore","Ujjain"]
+})
+
+value = pd.merge(df_departments,df_employees,on="department_id",how="right")
+print(df_employees)
+print(df_departments)
+
+print(value)
